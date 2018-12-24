198c5e9a9f1dd7f68b2b41f87d066025e50f84c3
Tomasz <knayder@outlook.com>
2018-12-24 01:21:37 +0100

    Added some offsets

--- src/Client Core/Game/GSuperMeatBoy.hpp
+++ src/Client Core/Game/GSuperMeatBoy.hpp
@@ -5,25 +5,29 @@
 class Player
 {
 public:
-	char pad1[132];		// +0
+	char pad1[0x84];	// +0
 	float x;			// +132
 	float y;			// +136
-	char pad2[16];		// +140
-	float replayX;		// +156
-	float replayY;		// +160
+	char pad2[0x16];	// +140
+	float velocityX;	// 0x9c
+	float velocityY;	// 0xA0
+	char pad3[0x71c];	// 0x7c0
+	Player** ghosts; //Array of pointers pointing to ghosts (Kurwa tablice wskaŸników do duszków xD)
+	// [Player + 0x9DC] + 0x2C  LICZBA DUSZKÓW
 };
 
 class Unk1
 {
 public:
-	char pad[24];		// +0
+	// [[[Unk1+0x3C] + 0x34C] + 0x6DC]+0x384   TIMER
+	char pad[0x18];		// +0
 	Player* player;		// +24
 };
 
 class GSuperMeatBoy
 {
 public:
-	char pad[100];	// +0
+	char pad[0x64];	// +0
 	Unk1* unk1;		// +100
 
 	static GSuperMeatBoy* get()
