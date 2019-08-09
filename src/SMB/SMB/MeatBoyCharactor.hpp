3b0969d160a6f2086d361406dd70476ef5d82f9b
Piotr Krupa <piotrkrupa06@gmail.com>
2019-08-09 04:26:56 +0200

    Minor change

--- src/SMB/SMB/MeatBoyCharactor.hpp
+++ src/SMB/SMB/MeatBoyCharactor.hpp
@@ -14,28 +14,28 @@ class MeatBoyCharactor //: public SceneObject2D
 {
 public:
 	MeatBoyCharactorVTable* vtable; // +0
-	char pad1[128];				// +4
-	Vector2f pos;				// +132
-	char pad2[0x10];			// +140
-	Vector2f velocity;			// +156 0x9C
-	char pad3[0x34];			// +164 
-	MeatBoyDisplay* display;	// +216
-	char pad4[1746];			// +220 
-	int animation1;				// +1968
-	int animation2;				// +1972
-	int animation3;				// +1976
-	int unk2;					// +1980
-	MeatBoyCharactor** ghosts;	// +1984 0x7C0 Array of pointers pointing to ghosts (Kurwa tablice wskaźników do duszków xD)
-	char pad5[88];				// +1988
-	Vector2f renderPos;			// +2076
-	char pad6[440];				// +2084
-	ReplayManager* replayMgr;	// +2524 0x9DC
-	char pad7[12];				// +2528
-	int animation4;				// +2540
-	int unk3;					// +2544
-	int characterType;			// +2548 default: 0 (DefaultMeatBoy)
-	char pad8[76];				// +2552
-	int ghostIndex;				// +2628 
+	char pad1[128];					// +4
+	Vector2f pos;					// +132
+	char pad2[0x10];				// +140
+	Vector2f velocity;				// +156 0x9C
+	char pad3[0x34];				// +164 
+	MeatBoyDisplay* display;		// +216
+	char pad4[1746];				// +220 
+	int animation1;					// +1968
+	int animation2;					// +1972
+	int animation3;					// +1976
+	int unk2;						// +1980
+	MeatBoyCharactor** ghosts;		// +1984 0x7C0 Array of pointers pointing to ghosts
+	char pad5[88];					// +1988
+	Vector2f renderPos;				// +2076
+	char pad6[440];					// +2084
+	ReplayManager* replayMgr;		// +2524 0x9DC
+	char pad7[12];					// +2528
+	int animation4;					// +2540
+	int unk3;						// +2544
+	int characterType;				// +2548 default: 0 (DefaultMeatBoy)
+	char pad8[76];					// +2552
+	int ghostIndex;					// +2628 
 	// +2552
 
 
